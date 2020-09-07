8b5c278e8cea479787c2d89931c5ceeecc112de1
korkies22 <axficionado@hotmail.com>
2020-09-06 20:32:46 -0500

    Agregado reto

---
+++ retoP3.js
@@ -0,0 +1,18 @@
+// Reto
+// Utilizando un algoritmo de fuerza bruta, imprima en consola la solución al siguiente problema:
+/**
+Beatriz, Carlos, Flor, Mario y Sonia viven cada uno en distintos pisos de un edificio de apartamentos que sólo tiene cinco pisos. 
+Beatriz no vive en el quinto piso. 
+Carlos no vive en el primer piso. 
+Flor no vive ni en el primer ni en el quinto piso. 
+Mario vive en un piso superior al piso en que vive Carlos. 
+Sonia no vive en un piso adyacente al de Flor.
+Flor no vive en un piso adyacente al de Carlos. 
+¿En qué piso vive cada uno?.
+*/
+//PISTA: la idea es resolverlo escribiendo 5 fors anidados
+
+//BONO: Resolver el ejercicio con lógica y escribir la solución:
+//Por ejemplo "Carlos no puede vivir ni en el 1° ni en el 5° piso porque..."
+
+
